32830e142c66bad2b943539a2fa61508666cd7d2
Suizzle <andygolay@gmail.com>
2023-12-16 14:53:27 -0500

    Update lib.rs
    Changed pancakes to coins and cat to dog.
    Removed `vel` field from `Coin` struct so coins don't move.
    Set coin positions to be at random places around the board. 
    Right now it's still generating coins one at a time... was thinking that could be fun.
    Did not add death coin yet.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 // Define the game configuration using the turbo::cfg! macro
 turbo::cfg! {r#"
-    name = "Pancake Cat"
+    name = "BONK Collecter"
     version = "1.0.0"
     author = "Turbo"
-    description = "Catch falling pancakes!"
+    description = "Collect the BONK before you bite the death coin! Death is temporary! Play again and again!"
     [settings]
     resolution = [256, 144]
     [solana]
@@ -16,13 +16,12 @@ turbo::init! {
     struct GameState {
         frame: u32,
         last_munch_at: u32,
-        cat_x: f32,
-        cat_y: f32,
-        cat_r: f32,
-        pancakes: Vec<struct Pancake {
+        dog_x: f32,
+        dog_y: f32,
+        dog_r: f32,
+        coins: Vec<struct Coin {
             x: f32,
             y: f32,
-            vel: f32,
             radius: f32,
         }>,
         score: u32,
@@ -30,10 +29,10 @@ turbo::init! {
         Self {
             frame: 0,
             last_munch_at: 0,
-            cat_x: 128.0,
-            cat_y: 112.0,
-            cat_r: 8.0,
-            pancakes: vec![],
+            dog_x: 128.0,
+            dog_y: 112.0,
+            dog_r: 8.0,
+            coins: vec![],
             score: 0,
         }
     }
@@ -46,49 +45,53 @@ turbo::go! {
 
     // Handle user input
     if gamepad(0).left.pressed() {
-        state.cat_x -= 2.;
+        state.dog_x -= 2.;
     }
     if gamepad(0).right.pressed() {
-        state.cat_x += 2.;
+        state.dog_x += 2.;
+    }
+    if gamepad(0).up.pressed() {
+        state.dog_y -= 2.;
+    }
+    if gamepad(0).down.pressed() {
+        state.dog_y += 2.;
     }
 
-    // Generate new pancakes at random intervals
+    // Generate new coins at random intervals
     if rand() % 64 == 0 {
-        // Create a new pancake with random attributes
-        let pancake = Pancake {
+        // Create a new coin with random attributes
+        let coin = Coin {
             x: (rand() % 256) as f32,
-            y: 0.0,
-            vel: (rand() % 3 + 1) as f32,
+            y: (rand() % 144) as f32,
             radius: (rand() % 10 + 5) as f32,
         };
-        state.pancakes.push(pancake);
+        state.coins.push(coin);
     }
 
-    // Update pancake positions and check for collisions with the cat
-    let cat_center = (state.cat_x + state.cat_r, state.cat_y + state.cat_r);
-    state.pancakes.retain_mut(|pancake| {
-        pancake.y += pancake.vel;
+    // Update coin positions and check for collisions with the dog
+    let dog_center = (state.dog_x + state.dog_r, state.dog_y + state.dog_r);
+    state.coins.retain_mut(|coin| {
 
-        // Check for collision with the cat
-        let pancake_center = (pancake.x + pancake.radius, pancake.y + pancake.radius);
+        // Check for collision with the dog
+        let coin_center = (coin.x + coin.radius, coin.y + coin.radius);
 
-        // Calculate the distance between the cat and the pancake
-        let dx = cat_center.0 - pancake_center.0;
-        let dy = cat_center.1 - pancake_center.1;
+        // Calculate the distance between the dog and the coin
+        let dx = dog_center.0 - coin_center.0;
+        let dy = dog_center.1 - coin_center.1;
 
         let distance = (dx * dx + dy * dy).sqrt();
-        let radii_sum = state.cat_r + pancake.radius;
-        let radii_diff = (state.cat_r - pancake.radius).abs();
+        let radii_sum = state.dog_r + coin.radius;
+        let radii_diff = (state.dog_r - coin.radius).abs();
 
         if radii_diff <= distance && distance <= radii_sum {
-            // Cat caught the pancake
+            // Dat caught the coin
             state.score += 1;
             state.last_munch_at = state.frame;
-            false // Remove the pancake from the game
-        } else if pancake.y < 144. + (pancake.radius * 2.) {
-            true // Keep the pancake in the game if it's within the screen
+            false // Remove the coin from the game
+        } else if coin.y < 144. + (coin.radius * 2.) {
+            true // Keep the coin in the game if it's within the screen
         } else {
-            false // Remove the pancake if it's off-screen
+            false // Remove the coin if it's off-screen
         }
     });
 
@@ -107,23 +110,23 @@ turbo::go! {
         }
     }
 
-    // Draw a speech bubble when the cat eats a pancake
+    // Draw a speech bubble when the dog eats a coin
     if state.frame >= 64 && state.frame.saturating_sub(state.last_munch_at) <= 60 {
-        rect!(w = 30, h = 10, x = state.cat_x as i32 + 32, y = state.cat_y as i32);
-        circ!(d = 10, x = state.cat_x as i32 + 28, y = state.cat_y as i32);
-        rect!(w = 10, h = 5, x = state.cat_x as i32 + 28, y = state.cat_y as i32 + 5);
-        circ!(d = 10, x = state.cat_x as i32 + 56, y = state.cat_y as i32);
-        text!("MUNCH!", x = state.cat_x as i32 + 33, y = state.cat_y as i32 + 3, font = Font::S, color = 0x000000ff);
+        rect!(w = 30, h = 10, x = state.dog_x as i32 + 32, y = state.dog_y as i32);
+        circ!(d = 10, x = state.dog_x as i32 + 28, y = state.dog_y as i32);
+        rect!(w = 10, h = 5, x = state.dog_x as i32 + 28, y = state.dog_y as i32 + 5);
+        circ!(d = 10, x = state.dog_x as i32 + 56, y = state.dog_y as i32);
+        text!("BONK!", x = state.dog_x as i32 + 33, y = state.dog_y as i32 + 3, font = Font::S, color = 0x000000ff);
     }
 
-    // Draw the cat
-    sprite!("munch_cat", x = (state.cat_x - state.cat_r) as i32, y = (state.cat_y - 16.) as i32, fps = fps::FAST);
+    // Draw the dog
+    sprite!("munch_dog", x = (state.dog_x - state.dog_r) as i32, y = (state.dog_y - 16.) as i32, fps = fps::FAST);
 
-    // Draw the falling pancakes
-    for pancake in &state.pancakes {
-        circ!(x = pancake.x as i32, y = pancake.y as i32 + 1, d = (pancake.radius + 2.) as u32, fill = 0x000000aa); // Render the pancakes
-        circ!(x = pancake.x as i32, y = pancake.y as i32, d = (pancake.radius + 1.) as u32, fill = 0xf4d29cff); // Render the pancakes
-        circ!(x = pancake.x as i32, y = pancake.y as i32, d = pancake.radius as u32, fill = 0xdba463ff); // Render the pancakes
+    // Draw the coins
+    for coin in &state.coins {
+        circ!(x = coin.x as i32, y = coin.y as i32 + 1, d = (coin.radius + 2.) as u32, fill = 0x000000aa); // Render the coins
+        circ!(x = coin.x as i32, y = coin.y as i32, d = (coin.radius + 1.) as u32, fill = 0xefff00ff); // Render the coins
+        circ!(x = coin.x as i32, y = coin.y as i32, d = coin.radius as u32, fill = 0xf1c232ff); // Render the coins
     }
 
     // Draw the score
